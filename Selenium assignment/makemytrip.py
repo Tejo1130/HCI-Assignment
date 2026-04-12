99823cbdbb405affb8247e4c0076eb318e6ec7bc
Tejo1130 <tejovardhan@outlook.com>
2026-04-13 01:50:06 +0530

    fixed sleep/wait issues

--- Selenium assignment/makemytrip.py
+++ Selenium assignment/makemytrip.py
@@ -3,9 +3,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--start-maximized")
+options.add_argument("--disable-notifications")
+
+service = Service("chromedriver.exe")
+driver = webdriver.Chrome(service=service, options=options)
+
 driver.maximize_window()
 
 steps = 0
